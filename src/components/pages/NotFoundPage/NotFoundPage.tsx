4445050d1e5b2a25f2965716fa211ec58f006203
Mihail <xp_92@mail.ru>
2023-05-05 12:40:41 +0500

    add NotFoundPage

---
+++ src/components/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,23 @@
+import { memo } from "react";
+
+import { Link } from "react-router-dom";
+
+import { Typography } from "@mui/material";
+
+import { ROUTES } from "@constants/routes";
+
+import styles from "./NotFoundPage.module.scss";
+
+const NotFoundPage = () => {
+    return (
+        <div className={styles.wrapper}>
+            <Typography variant="h3">Not found</Typography>
+
+            <Link to={ROUTES.MAIN}>
+                <Typography variant="button">Go Main page</Typography>
+            </Link>
+        </div>
+    );
+};
+
+export default memo(NotFoundPage);
